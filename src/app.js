64a180d7a0e774bc991c85ef424e99ede4794f5e
MarcelaZenteno93 <126695026+MarcelaZenteno93@users.noreply.github.com>
2023-10-16 16:31:42 +0000

    Terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,39 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const palos = ["♠", "♥", "♦", "♣"];
+
+  const generarCartaAleatoria = () => {
+    const numeroAleatorio = numeros[Math.floor(Math.random() * numeros.length)];
+    const paloAleatorio = palos[Math.floor(Math.random() * palos.length)];
+
+    const carta = document.getElementById("carta");
+    const palosElementos = carta.querySelectorAll(".palo");
+
+    // Actualizar ambos elementos de clase "palo" con el mismo palo aleatorio
+    palosElementos.forEach(paloElemento => {
+      paloElemento.textContent = paloAleatorio;
+    });
+
+    carta.querySelector(".numero").textContent = numeroAleatorio;
+  };
+
+  document
+    .getElementById("generarCarta")
+    .addEventListener("click", generarCartaAleatoria);
 };
